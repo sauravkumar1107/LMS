d4af227d848d815ce8ecc0bf608bad228750bc2c
sauravkumar1107 <46196100+sauravkumar1107@users.noreply.github.com>
2024-12-31 19:36:19 +0000

    Test restaurant/add API

--- lms/src/main/java/com/lms/lms/model/Restaurant.java
+++ lms/src/main/java/com/lms/lms/model/Restaurant.java
@@ -7,12 +7,14 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "restaurants")
 public class Restaurant {
