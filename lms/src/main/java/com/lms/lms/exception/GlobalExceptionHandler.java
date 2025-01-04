bf2c2b26d998abfca9528f63edb74eb3c5ef6fc7
Saurav Kumar <sauravkumar@Sauravs-MacBook-Air.local>
2025-01-05 01:55:27 +0530

    Refactor

--- lms/src/main/java/com/lms/lms/exception/GlobalExceptionHandler.java
+++ lms/src/main/java/com/lms/lms/exception/GlobalExceptionHandler.java
@@ -1,18 +1,33 @@
 package com.lms.lms.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGlobalException(Exception ex, WebRequest request) {
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex, WebRequest request) {
         log.error("Encountered exception, handleGlobalException", ex);
-        return new ResponseEntity<>("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Something went wrong",
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+}
+
+@Data
+@AllArgsConstructor
+class ErrorResponse {
+    private int status;
+    private String message;
+    private String details;
 }
