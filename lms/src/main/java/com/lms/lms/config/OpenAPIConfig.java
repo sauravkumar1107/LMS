5e67411c0224b851d875762dacd9ade2df607a93
Saurav Kumar <sauravkumar@Sauravs-MacBook-Air.local>
2025-01-05 02:12:15 +0530

    Refactor

--- lms/src/main/java/com/lms/lms/config/OpenAPIConfig.java
+++ lms/src/main/java/com/lms/lms/config/OpenAPIConfig.java
@@ -3,13 +3,22 @@ package com.lms.lms.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
 @Configuration
 public class OpenAPIConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(Arrays.asList(
+                        new Server().url("http://production-url").description("Production"),
+                        new Server().url("http://staging-url").description("Staging"),
+                        new Server().url("http://localhost:8090").description("Local")
+                ))
                 .info(new Info()
                         .title("Lead Management System API")
                         .version("1.0")
