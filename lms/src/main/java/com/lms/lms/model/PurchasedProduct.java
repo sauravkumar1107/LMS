23c0633cccc45dc6a56691359956b61719fbdd3a
Saurav Kumar <sauravkumar@Sauravs-MacBook-Air.local>
2025-01-04 22:59:13 +0530

    Refactor entities

--- lms/src/main/java/com/lms/lms/model/PurchasedProduct.java
+++ lms/src/main/java/com/lms/lms/model/PurchasedProduct.java
@@ -16,6 +16,9 @@ public class PurchasedProduct {
     @Id
     private String id;
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
     private Integer quantity;
